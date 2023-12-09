c728403679eb07ab7d79a0d4bd267563fddb654d
Andrey Shaporev <ashaporev@list.ru>
2023-12-09 18:49:37 +0400

    07.12.2023

--- 2/7/10.py
+++ 2/7/10.py
@@ -1,14 +1,23 @@
+# def permission():
+#     if stor_local.permission:
+#         if not hasattr(permission, '_count'):  # инициализация значения
+#             permission._count = {}
+#         if not hasattr(permission, '_flag'):
+#             permission._flag = True
+#         if threading.current_thread().name in permission._count:
+#             permission._count[threading.current_thread().name] += 1
+#             if permission._count[threading.current_thread().name] == 2 and permission._flag:
+#                 permission._flag = False
+#                 return True
+#         else:
+#             permission._count[threading.current_thread().name] = 1        
+#     return False
+
+
 def permission():
     if stor_local.permission:
-        if not hasattr(permission, '_count'):  # инициализация значения
-            permission._count = {}
-        if not hasattr(permission, '_flag'):
-            permission._flag = True
-        if threading.current_thread().name in permission._count:
-            permission._count[threading.current_thread().name] += 1
-            if permission._count[threading.current_thread().name] == 2 and permission._flag:
-                permission._flag = False
-                return True
+        if not hasattr(stor_local, '_count'):
+            stor_local._count = 1
         else:
-            permission._count[threading.current_thread().name] = 1        
+            return True
     return False
